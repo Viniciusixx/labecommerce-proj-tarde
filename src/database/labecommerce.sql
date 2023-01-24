aa83ffee176da85b2bb97077002108db9ecf7a8c
Viniciusixx <vinitotheworld@gmail.com>
2023-01-24 14:22:37 -0300

    segundo com

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,132 +1,161 @@
-
-
---relacoes sql I
-
-CREATE TABLE users( -- criar tabela
-    id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+-- Active: 1674579802581@@127.0.0.1@3306
+CREATE TABLE users (
+	id TEXT PRIMARY KEY UNIQUE NOT NULL,
+	email TEXT UNIQUE NOT NULL,
+	password TEXT NOT NULL
 );
 
+--Populando tabela de users
 INSERT INTO users(id, email, password)
-VALUES ("o001", "vini@gmail.com", "423r24"),
-("o002", "marcia@gmail.com", "42rf24r4"),
-("o003", "outlier@gmail.com", "245rf43t"),
-("o004", "bianca@gmail.com", "23524r42");
+VALUES("a001", "vini@gmail.com", "23x32r");
 
-SELECT * FROM users;
+INSERT INTO users(id, email, password)
+VALUES("a002", "santos@gmail.com", "2xr2zf2");
+
+INSERT INTO users(id, email, password)
+VALUES("a003", "sergiocortela@gmail.com", "ewxfw34t");
+
+
 
 --excluir tabela
 DROP TABLE users;
 
---retorna os usuários cadastrados
-SELECT ('users');
-
---mocke um termo de busca (email)
-SELECT * FROM users
-WHERE email;
-
---mocke uma id
-SELECT * FROM users
-WHERE id = "003";
-
---delete a linha baseada no valor mockado
-DROP TABLE users
-id = 3;
 
 
-
-
-CREATE TABLE products( -- criar tabela de produtos
-    id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    name TEXT NOT NULL,
-    price REAL NOT NULL ,
+CREATE TABLE products (
+id TEXT PRIMARY KEY UNIQUE NOT NULL,
+	name TEXT  NOT NULL,
+	price REAL NOT NULL,
     category TEXT NOT NULL
-);
-
---excluir tabela
-DROP TABLE products;
+ );
 
 INSERT INTO products(id, name, price, category)
-VALUES ("o001", "banana", 4, "feira"),
-("o002", "maça", 3, "feira"),
-("o003", "pera", 5, "feira"),
-("o004", "uva", 6, "feira"),
-("o005", "abacate", 12, "feira");
+VALUES("f001", "maça", 10, "feira");
 
-SELECT * FROM products;
-
---retorna os produtos cadastrados
-SELECT ('products');
---mocke um termo de busca (category)
-SELECT * FROM products
-WHERE category;
-
---mocke um termo de busca, por exemplo "monitor"
---retorna o resultado baseado no termo de busca
-SELECT * FROM products
-WHERE name = "computador";
-
---mocke uma products
-SELECT * FROM products
-WHERE id = "a003";
-
---mocke um novo usuário
---insere o item mockado na tabela users
 INSERT INTO products(id, name, price, category)
-VALUES
-("a006", "vassoura", 20, "feira");
+VALUES("f002", "pera", 12, "feira");
 
---mocke uma id
---delete a linha baseada no valor mockado
-DELETE FROM products
-WHERE id = "a002";
+INSERT INTO products(id, name, price, category)
+VALUES("f003", "uva", 5, "feira");
 
---mocke valores para editar um user
---edite a linha baseada nos valores mockados
+INSERT INTO products(id, name, price, category)
+VALUES("f004", "mandioca", 15, "feira");
+
+INSERT INTO products(id, name, price, category)
+VALUES("f005", "alface", 2, "feira");
+
+SELECT *  from products;
+
 UPDATE products 
-SET price = 40
-WHERE id = "a001";
+SET name = "bijus", price =200
+WHERE id = "f002";
 
-SELECT * FROM products;
+UPDATE products
+SET price = 200
+WHERE id = "f002";
 
---retorna o resultado ordenado pela coluna email em ordem crescente
-SELECT * FROM users ORDER by email ASC;
 
---retorna o resultado ordenado pela coluna price em ordem crescente
---limite o resultado em 20 iniciando pelo primeiro item
-SELECT * FROM products ORDER by price ASC LIMIT 20;
+SELECT *  from products;
+SELECT *  from users;
 
---mocke um intervalo de preços, por exemplo entre 100.00 e 300.00
---retorna os produtos com preços dentro do intervalo mockado em ordem crescente
-SELECT * FROM products 
-WHERE price >="4" AND price <="40" 
-ORDER by price ASC ;
+SELECT * FROM products
+WHERE name ="maça";
 
-DROP TABLE purchases;
+INSERT INTO users(id, email, password)
+VALUES("a006", "camargo@labenu.com", "8rggy687ht");
 
-SELECT * FROM purchases;
+
+INSERT INTO products(id, name, price, category)
+VALUES("f006", "açaí", 22, "feira");
+
+SELECT *  from products;
+
+SELECT * FROM products
+WHERE id= "f006";
+
+DELETE  FROM users
+WHERE id="a006";
+
+
+DELETE FROM products
+WHERE id = "a006";
+
+UPDATE users SET email="maraisa@gmail.com", password="4v3y6y4" WHERE id="a001";
+
+
+UPDATE products SET price=200 WHERE id="f002";
+
+SELECT * FROM users
+ORDER BY email asc;
+
+
+SELECT * FROM products
+ORDER BY price asc LIMIT 20;
+
+SELECT * FROM products
+WHERE price >= "2" AND price <="100"
+ORDER BY price ASC;
+
 
 CREATE TABLE purchases(
- id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    total_price REAL UNIQUE NOT NULL ,
-    paid INTEGER NOT NULL,
-    delivered_at TEXT,
-    buyer_id TEXT NOT NULL,
-     FOREIGN KEY (buyer_id) REFERENCES users (id)
+	id TEXT PRIMARY KEY UNIQUE NOT NULL,
+	total_price REAL UNIQUE NOT NULL, 
+	paid INTEGER NOT NULL,
+	delivered_at TEXT,
+	buyer_id TEXT NOT NULL, 
+	 FOREIGN KEY (buyer_id) REFERENCES users (id)
+ 
 );
 
 INSERT INTO purchases(id, total_price, paid, delivered_at, buyer_id)
-VALUES ("b001", 5000, 0, "", "001"),
-("b002", 1000, 0, "", "002"),
-("b003", 4000, 0,  "", "003"),
-("b004", 3000, 0, "", "001");
+VALUES("p001",10, 0,"","f001"),
+("p002",20, 0,"","f002"),
+("p003",30, 0,"","f001"),
+("p004",40, 0,"","f003");
+
+SELECT * FROM purchases;
+DROP TABLE purchases;
 
 UPDATE purchases
-SET delivered_at = "DATATIME 22/01/2023"
-WHERE id="b002";
+SET delivered_at=DATETIME("now", "localtime")
+WHERE id ="p001";
 
 SELECT * FROM purchases
 INNER JOIN users
-ON purchases.buyer_id=users.id
-WHERE users.id= "001";
+ON purchases.buyer_id =users.id
+WHERE users.id="a001";
+
+CREATE TABLE purchases_products (
+	purchase_id TEXT NOT NULL, 
+	product_id TEXT NOT NULL, 
+	quantity INTEGER NOT NULL,
+	FOREIGN KEY (purchase_id) REFERENCES purchases(id)
+	FOREIGN KEY (product_id) REFERENCES products(id)
+
+
+);
+INSERT INTO purchases_products(purchase_id, product_id, quantity)
+VALUES
+("p001", "f003", 1),
+("p002", "f003", 2),
+("p003", "f003", 3);
+DROP TABLE purchases_products;
+
+SELECT * FROM purchases
+INNER JOIN  purchases_products  
+ON  purchases_products.purchase_id =purchases.id
+;
+SELECT 
+purchases.id AS purchaseId,
+purchases.total_price, 
+purchases.paid,
+purchases.delivered_at, 
+purchases.buyer_id AS buyerId,
+products.id, 
+products.name,
+products.price
+FROM purchases
+LEFT JOIN purchases_products
+ON purchases_products.purchase_id = purchases.id
+INNER JOIN products
+ON  purchases_products.product_id = products.id;
--- src/index.ts
+++ src/index.ts
@@ -1,207 +1,292 @@
-import { users, products, purchases } from "./database";
+import { 
+    users,
+    products ,
+
+    purchases
+} from "./database";
 import cors from 'cors'
+
 import express, { Request, Response } from 'express'
-import { TProduct, TUser, TPurchase } from "./type";
+import { TProduct, TUser, TPurchase, CATEGORY_PURCHASE  } from "./type";
 
 const app = express();
-    app.use(express.json())
-    app.use(cors())
+app.use(express.json())
+app.use(cors())
 
-    app.listen(3003, () => {
+app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003")
 })
 
-    app.get("/users", (req: Request, res: Response) => {
+app.get("/users", (req: Request, res: Response) => {
     try {
-        res.status(200).send(users);
+      res.status(200).send(users);
     } catch (error: any) {
-        console.log(error);
-
-        if (res.statusCode === 200) {
-            res.status(500);
-        }
-
-        res.send(error.message);
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
     }
-});
-
+  });
 app.get("/products", (req: Request, res: Response) => {
     try {
-        res.status(200).send(products);
+      res.status(200).send(products);
     } catch (error: any) {
-        console.log(error);
-
-        if (res.statusCode === 200) {
-            res.status(500);
-        }
-
-        res.send(error.message);
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
     }
-});
-
-
+  });
+  
+    
 app.get("/product/search", (req: Request, res: Response) => {
     let productFilter;
     try {
-        const q = req.query.q as string;
-
-        if (q.length <= 1) {
-            res.status(400);
-            throw new Error("query params deve possuir pelo menos um caractere");
-        }
-
-        productFilter = products.filter((product) => {
-            return product.name.toLowerCase().includes(q.toLowerCase());
-        });
-        res.status(200).send(productFilter);
+      const q = req.query.q as string;
+  
+      if (q.length <= 1) {
+        res.status(400);
+        throw new Error("query params deve possuir pelo menos um caractere");
+      }
+  
+      productFilter = products.filter((product) => {
+        return product.name.toLowerCase().includes(q.toLowerCase());
+      });
+      res.status(200).send(productFilter);
     } catch (error: any) {
-        console.log(error);
-
-        if (res.statusCode === 200) {
-            res.status(500);
-        }
-
-        res.send(error.message);
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
     }
-});
+  });
 
-
+     
 app.post("/users", (req: Request, res: Response) => {
     try {
-        const id = req.body.id;
-        const email = req.body.email;
-        const password = req.body.password;
-
-        const findId = users.find((user) => user.id === id);
-
-        if (findId) {
-            res.status(400);
-            throw new Error("ID indisponivel");
-        }
-
-        const findEmail = users.find((user) => user.email === email);
-
-        if (findEmail) {
-            res.status(400);
-            throw new Error("email indisponivel");
-        }
-
-        const newUser: TUser = {
-            id,
-            email,
-            password,
-        };
-
-        users.push(newUser);
-        res.status(201).send("Usuario criado com sucesso");
+      const id = req.body.id;
+      const email = req.body.email;
+      const password = req.body.password;
+  
+      const findId = users.find((user) => user.id === id);
+  
+      if (findId) {
+        res.status(400);
+        throw new Error("ID indisponivel");
+      }
+  
+      const findEmail = users.find((user) => user.email === email);
+  
+      if (findEmail) {
+        res.status(400);
+        throw new Error("EMAIL indisponivel");
+      }
+  
+      const newUser: TUser = {
+        id,
+        email,
+        password,
+      };
+  
+      users.push(newUser);
+      res.status(201).send("Usuario criado com sucesso 😎");
     } catch (error: any) {
-        console.log(error);
-
-        if (res.statusCode === 200) {
-            res.status(500);
-        }
-
-        res.send(error.message);
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
     }
-});
+  });
 
 
 app.post("/products", (req: Request, res: Response) => {
     try {
-        const id = req.body.id;
-        const name = req.body.name;
-        const price = req.body.price;
-        const category = req.body.category;
-
-        const findId = products.find((product) => product.id === id);
-
-        if (findId) {
-            res.status(400);
-            throw new Error("ID indisponivel");
-        }
-
-        const newProduct: TProduct = {
-            id,
-            name,
-            price,
-            category,
-        };
-
-        products.push(newProduct);
-        res.status(201).send("Produto criado com sucesso");
+      const id = req.body.id;
+      const name = req.body.name;
+      const price = req.body.price;
+      const category = req.body.category;
+  
+      const findId = products.find((product) => product.id === id);
+  
+      if (findId) {
+        res.status(400);
+        throw new Error("ID indisponivel");
+      }
+  
+      const newProduct: TProduct = {
+        id,
+        name,
+        price,
+        category,
+      };
+  
+      products.push(newProduct);
+      res.status(201).send("Produto criado com sucesso 😎");
     } catch (error: any) {
-        console.log(error);
-
-        if (res.statusCode === 200) {
-            res.status(500);
-        }
-
-        res.send(error.message);
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
     }
-});
+  });
 
-app.post("/purchase", (req: Request, res: Response) => {
-
+app.post("/purchase", (req:Request, res:Response)=>{
+   
+   try {
+    const userId= req.body.userId
+    const productId=req.body.productId
+    const quantity= req.body.quantity
+    const totalPrice= req.body.totalPrice
+  
+  const newPurchase: TPurchase ={userId, productId, quantity, totalPrice}
+  purchases.push(newPurchase)
+  res.status(201).send("Compra realizada com sucesso")
+   
+   } catch (error) {
+    
+   }
+  })
+  app.post("/purchases", (req: Request, res: Response) => {
     try {
-        const userId = req.body.userId
-        const productId = req.body.productId
-        const quantity = req.body.quantity
-        const totalPrice = req.body.totalPrice
-
-        const newPurchase: TPurchase = { userId, productId, quantity, totalPrice }
-        purchases.push(newPurchase)
-        res.status(201).send("Compra realizada com sucesso")
-
-    } catch (error) {
-
+      const userId = req.body.userId;
+      const productId = req.body.productId;
+      const quantity = req.body.quantity;
+      const totalPrice = req.body.totalPrice;
+  
+      const findIdUser = purchases.find((purchase) => purchase.userId === userId);
+  
+      if (!findIdUser) {
+        res.status(400);
+        throw new Error("ID do usuario não existe");
+      }
+  
+      const findIdProduct = products.find(
+        (product) => product.id === productId
+      );
+  
+      if (!findIdProduct) {
+        res.status(400);
+        throw new Error("ID do produto não existe");
+      }
+  
+      
+      if (findIdProduct.price * quantity !== totalPrice) {
+          res.status(400)
+          throw new Error("Total incorreto");
+      }
+  
+      const newPurchase: TPurchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice,
+      };
+  
+      purchases.push(newPurchase);
+      res.status(201).send("Compra efetuada com sucesso 😎");
+    } catch (error: any) {
+      console.log(error);
+  
+      if (res.statusCode === 200) {
+        res.status(500);
+      }
+  
+      res.send(error.message);
     }
+  });
+  app.get("/products/:id", (req: Request, res: Response)=>{
+    const id = req.params.id
+    
+const result = products.find((product)=>product.id ===id)
+res.status(200).send("objeto product encontrado")
 })
-app.post("/purchases", (req: Request, res: Response) => {
-    try {
-        const userId = req.body.userId;
-        const productId = req.body.productId;
-        const quantity = req.body.quantity;
-        const totalPrice = req.body.totalPrice;
 
-        const findIdUser = purchases.find((purchase) => purchase.userId === userId);
+app.get("/users/:id/purchase", (req: Request, res: Response)=>{
+    const id = req.params.id
+    
+const result = purchases.filter((purchase)=>purchase.userId ===id)
+res.status(200).send(result )
+})
 
-        if (!findIdUser) {
-            res.status(400);
-            throw new Error("ID do usuario não existe");
-        }
-
-        const findIdProduct = products.find(
-            (product) => product.id === productId
-        );
-
-        if (!findIdProduct) {
-            res.status(400);
-            throw new Error("ID do produto não existe");
-        }
-
-
-        if (findIdProduct.price * quantity !== totalPrice) {
-            res.status(400)
-            throw new Error("Total incorreto");
-        }
-
-        const newPurchase: TPurchase = {
-            userId,
-            productId,
-            quantity,
-            totalPrice,
-        };
-
-        purchases.push(newPurchase);
-        res.status(201).send("Compra efetuada com sucesso");
-    } catch (error: any) {
-        console.log(error);
-
-        if (res.statusCode === 200) {
-            res.status(500);
-        }
-
-        res.send(error.message);
+app.delete("/users/:id", (req: Request, res: Response)=>{
+    const id = req.params.id
+    const indexToRemove= users.findIndex((user)=>user.id ===id)
+    if(indexToRemove >= 0){
+        users.splice(indexToRemove, 1)
     }
-});
+    res.status(200).send( "User apagado com sucesso")
+    })
 
+    app.delete("/products/:id", (req: Request, res: Response)=>{
+        const id = req.params.id
+        const indexToRemove= products.findIndex((product)=>product.id ===id)
+        if(indexToRemove >= 0){
+            products.splice(indexToRemove, 1)
+        }
+        res.status(200).send( "Produto apagado com sucesso")
+        })
+
+ 
+        app.put("/users/:id", (req: Request, res: Response)=>{
+            const id = req.params.id
+            const newId = req.body.id as string | undefined
+            const newEmail = req.body.email as string | undefined
+            const newPassword = req.body.password as string |undefined
+
+            
+    
+            const resultToEdit = users.find((user)=> user.id ===id)
+            if(resultToEdit){
+              
+    resultToEdit.id=newId || resultToEdit.id
+    resultToEdit.email=newEmail || resultToEdit.email
+    resultToEdit.password=newPassword || resultToEdit.password
+
+
+   
+    
+            }
+            res.status(200).send("Atualização realizada com sucesso")
+    
+        })
+
+        app.put("/product/:id", (req: Request, res: Response)=>{
+            const id = req.params.id
+            const newId = req.body.id as string | undefined
+            const newName = req.body.name as string | undefined
+            const newPrice = req.body.price as number 
+            const newCategory = req.body.category as  CATEGORY_PURCHASE | undefined
+
+            
+    
+            const resultToEdit = products.find((product)=> product.id ===id)
+            if(resultToEdit){
+              
+    resultToEdit.id=newId || resultToEdit.id
+    resultToEdit.name=newName || resultToEdit.name
+    resultToEdit.price = isNaN(newPrice) ?  resultToEdit.price : newPrice
+    resultToEdit.category=newCategory || resultToEdit.category
+
+
+
+   
+    
+            }
+            res.status(200).send("Produto atualizado com sucesso")
+    
+        })
